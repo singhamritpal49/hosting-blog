090b9590b50fd055d1defc6f4965f53538cbbe64
Amritpal Singh <singhamritpal49@gmail.com>
2020-03-22 17:13:21 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! 🌎 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-link" >
+          
+          Let's Deploy this React Application  to Heroku
+        </p>
       </header>
     </div>
   );
